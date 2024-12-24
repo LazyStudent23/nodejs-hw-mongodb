0affa9175e0d969588a690e412688d18bcf92c53
LazyStudent23 <wizard.alex2016@gmail.com>
2024-12-24 14:33:23 +0200

    fix error handlers

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 export const errorHandler = (error, req, res, next) => {
   const { status = 500, message } = error;
   res.status(status).json({
-   status: 500,
+   status: 404,
 		message: "Something went wrong",
 		data: message,
   });
